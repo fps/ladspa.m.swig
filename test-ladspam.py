0b9aae2b0edba5fd38f1efd861f19e6ca7d6bb5c
Florian Paul Schmidt <mista.tapas@gmx.net>
2013-03-31 15:57:42 +0200

    changes

--- test-ladspam.py
+++ test-ladspam.py
@@ -1,8 +1,8 @@
 import ladspam0
 import time
 
-m = ladspam0.m_jack("m")
+m = ladspam0.m_jack("m", 8)
 m.insert_plugin(0, "/usr/lib/ladspa/flanger_1191.so", "flanger")
 
-time.sleep(1)
+time.sleep(5)
 
